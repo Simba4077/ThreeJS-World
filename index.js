06a46ff81f9d7f09dab18de097717fada52a7e52
Mira Saini <mirasaini1005@icloud.com>
2026-03-10 14:45:07 -0700

    added texture to center cube

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 
 // Camera setup --------------------------------------------------
 //camera default to looking across -z axis, with +y axis facing up
-const fov = 80;
+const fov = 100;
 const aspect = 2; // the canvas default
 const near = 0.1;
 const far = 5;
@@ -12,6 +12,12 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 //-----------------------------------------------------------------
 
+// Texture loader -------------------------------------------------
+const loader = new THREE.TextureLoader();
+const texture = loader.load('wall.jpg')
+texture.colorSpace = THREE.SRGBColorSpace;
+
+// ----------------------------------------------------------------
 // Scene, canvas, and renderer setup ------------------------------
 const scene = new THREE.Scene();
 const canvas = document.getElementById("myCanvas");
@@ -26,7 +32,7 @@ const boxGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 //-----------------------------------------------------------------
 
 // Material -------------------------------------------------------
-const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
+const material = new THREE.MeshPhongMaterial({map: texture});
 //-----------------------------------------------------------------
 
 //Lighting --------------------------------------------------------
@@ -37,15 +43,27 @@ light.position.set(-1,2,4);
 //-----------------------------------------------------------------
 
 // Mesh -----------------------------------------------------------
-const cube = new THREE.Mesh(boxGeometry, material);
-
 //call makeInstance 3 times and story Mesh instances in an array
 const cubes = [
-    makeInstance(boxGeometry, 0x44aa88, 0),
+    makeInstanceTexture(boxGeometry, material, 0),
     makeInstance(boxGeometry, 0x8844aa, -2),
     makeInstance(boxGeometry, 0xaa8844, 2),
 ];
 //-----------------------------------------------------------------
+
+function makeInstanceTexture(geometry, texture, x){
+
+    //make mesh from specified geometry and material
+    const cube = new THREE.Mesh(geometry, material);
+
+    //add to scene
+    scene.add(cube);
+
+    //set position
+    cube.position.x = x;
+
+    return cube;
+}
 
 function makeInstance(geometry, color, x){
     //make material from specified color
@@ -65,9 +83,6 @@ function makeInstance(geometry, color, x){
 
 function render(time){
     time *= 0.001;
-    
-    cube.rotation.x = time;
-    cube.rotation.y = time;
 
     renderer.render(scene, camera);
 
@@ -85,7 +100,6 @@ function render(time){
 function main(){
 
     //add mesh to scene
-    scene.add(cube);
     scene.add(light);
 
     //render scene
